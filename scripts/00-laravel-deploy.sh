7e545b12d7112e7350455ed35668b9dc34cfe564
SebastianSiuol <sebastianlouismtorio@iskolarngbayan.pup.edu.ph>
2024-12-09 08:04:01 +0800

    Add Deployment Files

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+echo "Running composer"
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Clearing existing config caches"
+php artisan config:clear
+
+echo "Clearing existing route caches"
+php artisan route:clear
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running in-development migrations"
+php artisan migrate:fresh --force
+
+#echo "Running npm build..."
+#npm run build --prefix /var/www/html
+
+#echo "Running migrations..."
+#php artisan migrate --force
