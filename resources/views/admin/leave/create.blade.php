9f6f3748e0ec0dcdb14d333ee8eb5d5a56ce7953
SebastianSiuol <sebastianlouismtorio@iskolarngbayan.pup.edu.ph>
2025-01-03 02:25:20 +0800

    Packages Updated

--- resources/views/admin/leave/create.blade.php
+++ resources/views/admin/leave/create.blade.php
@@ -93,7 +93,7 @@
 
                                 <x-table.data>
                                     <div id="buttons_{{$leave->id}}" {{ $leave->status == 'pending' ? "class=block" : "class=hidden" }}>
-                                        <button id="approve_leave"
+                                        <button id="approve_leave"A
                                                 type="button"
                                                 data-userId="{{$leave->id}}"
                                                 class="approve_leave text-white items-center justify-between bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-2 py-2 me-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">
--- resources/views/components/admin/configuration/shift/create.blade.php
+++ resources/views/components/admin/configuration/shift/create.blade.php
@@ -104,8 +104,8 @@
                         <x-forms.label for="shift_description" label_name="Shift Description"/>
                         <textarea id="shift_description"
                                   name="shift_description"
-                                  rows="4"
-                                  class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Description..." required></textarea>
+                                  rows="4"block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500
+                                  class="" placeholder="Description..." required></textarea>
                     </div>
 
 
