88fe27a89d7d78636e2e25e196c266caeb9c1448
SebastianSiuol <sebastianlouismtorio@iskolarngbayan.pup.edu.ph>
2025-02-24 03:16:01 +0800

    Add two new mailable class [Account Created, Reset Password]

---
+++ app/Mail/FacultyAccountCreated.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Faculty;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Queue\SerializesModels;
+
+class FacultyAccountCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($payload)
+    {
+        $this->data = $payload;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        $from_address = config('mail.from.address');
+        $from_name = config('mail.from.name');
+
+        return new Envelope(
+            from: new Address($from_address, $from_name),
+            subject: 'Faculty Account Created',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mails.account-creation',
+            with: [
+                'data' => $this->data,
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
