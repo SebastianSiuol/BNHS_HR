f37a654a65946f17eeaae145f379cc22448735e7
SebastianSiuol <sebastianlouismtorio@iskolarngbayan.pup.edu.ph>
2025-02-23 23:18:20 +0800

    Remove General Role

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -72,11 +72,7 @@ class RoleSeeder extends Seeder
                 'type' => 'hr',
                 'description' => 'Manager HR',
             ],
-            [
-                'role_name' => 'general_faculty',
-                'type' => 'general',
-                'description' => 'Faculty',
-            ],
+
         ];
 
         foreach($roles as $role){
